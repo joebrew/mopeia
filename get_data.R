3e7faabff13703688e6ad86501880f69a7695fe3
joebrew <joebrew@gmail.com>
2017-09-21 16:04:02 +0200

    dashbaord round 2

--- get_data.R
+++ get_data.R
@@ -180,6 +180,11 @@ visit_dates <-
   mutate(date = ifelse(date < '2016-06-01', date + lubridate::years(1), date)) %>% 
   mutate(date = as.Date(date, origin = '1970-01-01'))
 
+# Remove all visit dates prior to 2017, since they appear incorrect
+visit_dates <- visit_dates %>%
+  filter(date >= '2017-01-01')
+
+
 # Expand each child to total time observed
 expand_child <- function(visit_dates,
                          perm_id = '004-7060-004-0001'){
@@ -206,11 +211,16 @@ if(expanded_name %in% dir('backups')){
   load(paste0('backups/', expanded_name))
 } else {
   results <- list()
-  for(i in 1:nrow(visit_dates)){
+  all_pids <- sort(unique(visit_dates$permid))
+  for(i in 1:length(all_pids)){
+    this_perm_id <- all_pids[i]
     message(i)
-    results[[i]] <- 
-      expand_child(visit_dates = visit_dates,
-                   perm_id = visit_dates$permid[i])
+    expanded <- expand_child(visit_dates = visit_dates,
+                             perm_id = this_perm_id)
+    message(': ', length(which(expanded$visit)))
+    results[[i]] <- expanded
+      
+    
   }
   etar <- bind_rows(results)
   etar <- etar %>%
@@ -230,36 +240,148 @@ visit_dates$visit_done <- ifelse(visit_dates$x == 1,
                                         FALSE, NA))
 visit_dates$x <- NULL
 
-# ACD ########################
-# Round Incidence (seven of them)
-# (# of RDT+ that round* / # of children in cohort that round)
-#   * Round: equals visits of ACD. We are currently at the end of the 8th monthly follow up
+prettify <-
+  function (the_table, remove_underscores_columns = TRUE, cap_columns = TRUE, 
+            cap_characters = TRUE, comma_numbers = TRUE, date_format = "%B %d, %Y", 
+            round_digits = 2, remove_row_names = TRUE, remove_line_breaks = TRUE, 
+            data_table = TRUE, nrows = 8, download_options = TRUE) 
+  {
+    column_names <- names(the_table)
+    the_table <- data.frame(the_table)
+    names(the_table) <- column_names
+    classes <- lapply(the_table, function(x) {
+      unlist(class(x))[1]
+    })
+    if (cap_columns) {
+      names(the_table) <- Hmisc::capitalize(names(the_table))
+    }
+    if (remove_underscores_columns) {
+      names(the_table) <- gsub("_", " ", names(the_table))
+    }
+    for (j in 1:ncol(the_table)) {
+      the_column <- the_table[, j]
+      the_class <- classes[j][1]
+      if (the_class %in% c("character", "factor")) {
+        if (cap_characters) {
+          the_column <- as.character(the_column)
+          the_column <- Hmisc::capitalize(the_column)
+        }
+        if (remove_line_breaks) {
+          the_column <- gsub("\\n", " ", the_column)
+        }
+      }
+      else if (the_class %in% c("POSIXct", "Date")) {
+        the_column <- format(the_column, format = date_format)
+      }
+      else if (the_class %in% c("numeric", "integer")) {
+        the_column <- round(the_column, digits = round_digits)
+        if (comma_numbers) {
+          the_column <- scales::comma(the_column)
+        }
+      }
+      the_table[, j] <- the_column
+    }
+    if (remove_row_names) {
+      row.names(the_table) <- NULL
+    }
+    if (data_table) {
+      if (download_options) {
+        the_table <- DT::datatable(the_table, options = list(pageLength = nrows, 
+                                                             dom = "Bfrtip", buttons = list("copy", "print", 
+                                                                                            list(extend = "collection", buttons = "csv", 
+                                                                                                 text = "Download"))), rownames = FALSE, extensions = "Buttons")
+      }
+      else {
+        the_table <- DT::datatable(the_table, options = list(pageLength = nrows, 
+                                                             dom = 't',
+                                                             columnDefs = list(list(className = "dt-right", 
+                                                                                    targets = 0:(ncol(the_table) - 1)))), rownames = FALSE)
+      }
+    }
+    return(the_table)
+  }
 
-# x <- visit_dates %>%
-#   group_by(round) %>%
-#   summarise(children = n(),
-#             )
+date_truncate <- function (date_object, level = c("month", "quarter", "year")) {
+    if (is.null(level)) {
+      stop('Must provide month quarter year')
+    }
+    date_object <- as.Date(date_object)
+    if (sum(!is.na(date_object)) == 0) {
+      return(date_object)
+    }
+    if (level == "month") {
+      return_object <- date_object
+      return_object[!is.na(return_object)] <- as.Date(paste0(format(return_object[!is.na(return_object)], 
+                                                                    "%Y-%m"), "-01"))
+      return(return_object)
+    }
+    if (level == "quarter") {
+      q_month <- (((((as.numeric(format(date_object, "%m"))) - 
+                       1)%/%3) + 1) * 3) - 2
+      return_object <- date_object
+      return_object[!is.na(return_object)] <- as.Date(paste0(format(return_object[!is.na(return_object)], 
+                                                                    "%Y"), ifelse(nchar(q_month[!is.na(return_object)]) == 
+                                                                                    2, "-", "-0"), q_month, "-01"))
+      return(return_object)
+    }
+    if (level == "year") {
+      return_object <- date_object
+      return_object[!is.na(return_object)] <- as.Date(paste0(format(return_object[!is.na(return_object)], 
+                                                                    "%Y"), "-01-01"))
+      return(return_object)
+    }
+  }
 
+# Combine the two pcds
+pcd <- 
+  bind_rows(pcd %>% mutate(village = as.character(village)),
+            pcd_v62017)
 
+# Clean up date in pcd
+pcd$date <- as.Date(pcd$visit_date)
 
-#   Cumulative incidence
-#   (Total # of RDT+ in the cohort since start / # of child-years at risk**)
-#     ** number of children in the cohort x time they have been followed. If a child was LFU then he
-#     contributed until that moment to child-time-at-risk
+# Get malaria status in pcd
+pcd <- pcd %>%
+  mutate(rdt = ifelse(tdr_res == '1', 'Positive',
+                      ifelse(tdr_res == '2', 'Negative', NA))) %>%
+  mutate(malaria = ifelse(tdr_res == '1', TRUE, FALSE))
 
+# Get spray status
+pcd <-
+  left_join(pcd %>% mutate(village = as.numeric(as.character(village))),
+            ss,
+            by = c('village' = 'village_number'))
 
-#     Cumulative incidence by spray status
-#     As above but disaggregated by those in “spray” and “no-spray” areas.
-#     Please add RR and 95%CI for the RR
-#     Corrected incidence 1
-#     Cumulative incidence BUT every time a child is positive we should remove 30 days of time-at-risk from
-#     the denominator
-#     Corrected incidence 2
-#     Cumulative incidence BUT every time a child is RDT+ we should remove 30 days of time-at-risk from the
-#     denominator and disregard the results of the immediate next test (as if it had not been done)
-#     Cumulative incidence by spray status AND Net usage
-#     As above but also disaggregated by sleep last night (question 9 of follow questionnaire)
-#     
+# Keep only 0-4 year olds
+pcd <- pcd %>%
+  filter(age == '1')
+
+# Keep only realistic dates
+pcd <-
+  pcd %>%
+  filter(date >= '2016-12-01',
+         date <= '2017-08-31')
+
+# Get health facility
+pcd$health_facility <- pcd$hf_id
+
+# Clean up health facility id
+pcd <-
+  pcd %>%
+  mutate(health_facility = as.numeric(health_facility)) %>%
+  mutate(health_facility = ifelse(health_facility %in% c(1, 17), 'Mopeia Sede',
+                                  ifelse(health_facility %in% c(2, 27), '8 de Marco',
+                                         ifelse(health_facility %in% c(3, 37), 'Sangalaza',
+                                                ifelse(health_facility %in% c(4, 47), 'Gulamo',
+                                                       ifelse(health_facility %in% c(5, 57), 'Luala',
+                                                              ifelse(health_facility %in% c(6, 67),  'Chimuara',
+                                                                     ifelse(health_facility %in% c(7, 77), 'Noere',
+                                                                            ifelse(health_facility %in% c(8, 87), 'Nhacatundo',
+                                                                                   ifelse(health_facility %in% c(9, 97), 'Nzanza', ifelse(health_facility %in% c(10, 18), 'Posto Campo', 
+                                                                                                                                          ifelse(health_facility %in% c(11, 19), 'Mungane',
+                                                                                                                                                 ifelse(health_facility %in% c(12, 28), 'Catale', NA)))))))))))))
+
+pcd <- pcd %>% filter(!is.na(health_facility))
 #     PCD
 #     Monthly Incidence in children under 5 (nine of them)
 #     (# of <5s RDT+ that month / # of children under 5 that live in areas served by that HF (according to
